30ea10daf729e26b7535e0081ee1651dfa8648d9
Michael McMullin <bluearkdesign@gmail.com>
2022-06-21 13:26:51 +0100

    Basic user entity

---
+++ Streaker.Core/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Streaker.SharedKernel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace Streaker.Core.Entities
+{
+    /// <summary>
+    /// Information about a user on whose behalf the code is running.
+    /// </summary>
+    public class User : EntityBase<int>, IIdentity
+    {
+        public string? Name { get; set; }
+
+        [NotMapped]
+        public string? AuthenticationType { get; set; }
+
+        [NotMapped]
+        public bool IsAuthenticated { get; set; }
+    }
+}
---
+++ Streaker.SharedKernel/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Streaker.SharedKernel
+{
+    /// <summary>
+    /// Base class for entities.
+    /// </summary>
+    /// <typeparam name="T">The type to use for the entity's ID. This is often an <see cref="int" /> but is flexible
+    /// enough to support other types, such as <see cref="Guid"/> or <see cref="string"/>.</typeparam>
+    public abstract class EntityBase<T> where T : notnull
+    {
+        /// <summary>
+        /// Gets or sets the unique ID of this entity.
+        /// </summary>
+        public T Id { get; set; }
+    }
+}
